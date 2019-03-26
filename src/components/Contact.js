c7edac53024d2e42f63e2129cdad511063dd14cb
Mohamed Osama <mohamed.osama98x@gmail.com>
2019-03-26 23:36:58 +0200

    Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Amira Ahmed</h4>
+        <ul>
+          <li>email: Amira@gmail.com</li>
+          <li>phone number: 01015923533</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;

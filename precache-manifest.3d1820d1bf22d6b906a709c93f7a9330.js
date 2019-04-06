a661b5c96835138fc67b66a682fe5b841c6112cf
Mohamed Osama <mohamed.osama98x@gmail.com>
2019-04-06 03:55:05 +0200

    Updates

---
+++ precache-manifest.3d1820d1bf22d6b906a709c93f7a9330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49225faa5712f33c0f72",
+    "url": "/contactManager/static/js/runtime~main.63e620a2.js"
+  },
+  {
+    "revision": "5476b6f4b7f706f612e3",
+    "url": "/contactManager/static/js/main.7859d548.chunk.js"
+  },
+  {
+    "revision": "72afa16144a02d64f63b",
+    "url": "/contactManager/static/js/2.c55792ab.chunk.js"
+  },
+  {
+    "revision": "5476b6f4b7f706f612e3",
+    "url": "/contactManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "72afa16144a02d64f63b",
+    "url": "/contactManager/static/css/2.edd4cd80.chunk.css"
+  },
+  {
+    "revision": "bd65118786d91ffc2911f0fd4a7f8041",
+    "url": "/contactManager/index.html"
+  }
+];
